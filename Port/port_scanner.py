ad5e3c900e4fbed4b49c69889f9fdbe1f611feea
MidnightSun27 <54239122+MidnightSun27@users.noreply.github.com>
2020-06-09 14:41:02 +0500

    Update port_scanner.py

--- Port/port_scanner.py
+++ Port/port_scanner.py
@@ -1,22 +1,29 @@
-import socket
+import asyncio
 import sys
 
 
-def port_scanner(left, right, _ip='localhost'):
-    for port in range(left, right):
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        with sock:
-            if not sock.connect_ex((_ip, port)):
-                print(f'Port {port} is open')
-            else:
-                print(f'Port {port} is close')
+async def checker(port, _ip='localhost'):
+    try:
+        reader, writer = await asyncio.co(_ip, port)
+
+        print(f'Port {port} is open')
+        writer.close()
+    except Exception:
+        print(f'Port {port} is close')
+
+
+async def port_scanner(left, right):
+    for port in range(left, right + 1):
+        await checker(port)
 
 
 def main():
     lo, hi = 0, 65_535
     left, right = tuple(map(int, sys.argv[1:]))
     if lo <= left and right <= hi:
-        port_scanner(left, right)
+        loop = asyncio.get_event_loop()
+        tasks = [loop.create_task(checker(border_port)) for border_port in range(left, right)]
+        loop.run_until_complete(asyncio.wait(tasks))
     else:
         print(f'Range [{left}, {right}] is incorrect.')
 
